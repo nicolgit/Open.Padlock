134d04d8b63921ecfa9231545b9a3f70b5d2d18c
NicolGit @ PC <nicolgit@users.noreply.github.com>
2019-10-03 18:35:45 +0200

    scraffolding with visualstudio

---
+++ nicold.Padlock/nicold.Padlock/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace nicold.Padlock
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
