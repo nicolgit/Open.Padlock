86875d53e20603686902d8ab8e7b3ff4d610007d
NicolGit @ PC <nicolgit@users.noreply.github.com>
2019-11-20 21:16:37 +0100

    Search also in row content

--- nicold.Padlock/nicold.Padlock.Models/DataFile/Card.cs
+++ nicold.Padlock/nicold.Padlock.Models/DataFile/Card.cs
@@ -21,7 +21,6 @@ namespace nicold.Padlock.Models.DataFile
         public DateTime LastOpenedDateTime { get; set; }
         public int UsedCounter { get; set; }
         public List<string> Tags { get; set; }
-
         public List<Attribute> Rows { get; set; }
 
         public override string ToString()
@@ -31,6 +30,32 @@ namespace nicold.Padlock.Models.DataFile
             sb.Append(Title);
             sb.Append(" ");
             sb.Append(Notes);
+            
+            foreach (var tag in Tags)
+            {
+                sb.Append(tag);
+                sb.Append(" ");
+            }
+
+            foreach (var row in Rows)
+            {
+                switch (row.Type)
+                {
+                    case AttributeType.TYPE_PASSWORD:
+                        // do not search by password value
+                        break;
+                    case AttributeType.TYPE_HEADER:
+                        sb.Append(row.Name);
+                        break;
+                    default:
+                    case AttributeType.TYPE_URL:
+                    case AttributeType.TYPE_STRING:
+                        sb.Append(row.Value);
+                        break;
+                }
+                sb.Append(" ");
+            }
+
             return sb.ToString().ToLower();
         }
     }
